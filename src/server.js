8cf65c8de19bb6a83e0b1c95c445fd4f2b1c75ae
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-22 16:13:10 -0700

    Created basic start function with app.listen call

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Import dependencies
+const express = require('express');
+const dotenv = require('dotenv'); // eslint-disable-line
+
+// Use dependencies
+const app = express();
+
+// Import modules
+const logger = require('./middleware/logger.js');
+const validator = require('./middleware/validator.js');
+const handler404 = require('./error-handlers/404.js');
+const handler500 = require('./error-handlers/500.js');
+
+// start function
+function start(PORT) {
+  app.listen(PORT, () => {
+    console.log('Server listening!');
+  });
+}
+
+module.exports = {
+  start: start,
+};
