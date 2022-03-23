1c58ff7c212b6d3bf9aad8e8798da853f4dae4e7
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-22 17:22:04 -0700

    Added bad route and bad method tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,10 +4,16 @@ const supertest = require('supertest');
 const { app } = require('../src/server.js');
 const request = supertest(app);
 
-describe('Testing server methods', () => {
+describe('Testing server handlers', () => {
 
   it('API should return 404 on a bad method', async () => {
     const response = await request.post('/');
+
+    expect(response.status).toEqual(404);
+  });
+
+  it('API should return 404 on a bad route', async () => {
+    const response = await request.get('/');
 
     expect(response.status).toEqual(404);
   });
