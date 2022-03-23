ec0ab4039e2655a1920d31c7ad90e3dd3e4b62d7
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-22 17:03:03 -0700

    Added validation logic to validator

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,7 +1,11 @@
 'use strict';
 
-function validator() {
-
+function validator(req, res, next) {
+  if (req.query.name === undefined) {
+    console.error('Received query with no "name"');
+    next(new Error);
+  }
+  else next();
 }
 
 module.exports = validator;
