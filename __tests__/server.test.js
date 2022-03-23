e7d57ed04329aae1d86ba2cd204168dbd8e1bf1d
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-22 17:10:38 -0700

    Created test for 404 on bad method

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const supertest = require('supertest');
+const { app } = require('../src/server.js');
+const request = supertest(app);
+
+describe('Testing server methods', () => {
+
+  it('API should return 404 on a bad method', async () => {
+    const response = await request.post('/');
+
+    expect(response.status).toEqual(404);
+  });
+});
