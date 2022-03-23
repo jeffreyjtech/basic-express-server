5fdcb3f6712cfc60ab095742f862538d044a8423
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-22 17:22:20 -0700

    Added validator tests for good and bad requests

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const supertest = require('supertest');
+const { app } = require('../src/server.js');
+const request = supertest(app);
+
+describe('Testing validator handler', () => {
+
+  it('API should return 200 with a correct object if request is good', async () => {
+    let url = '/person?name=Jeffrey';
+    const response = await request.get(url);
+
+    expect(response.status).toEqual(200);
+    
+  });
+
+  it('API should return 500 if request query is missing name property', async () => {
+    const response = await request.get('/person');
+
+    expect(response.status).toEqual(500);
+  });
+});
